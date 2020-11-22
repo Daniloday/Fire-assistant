552da4c3bfa84b806ea064047d7d7cf07c9956ee
daniloday <danil.vladyka1@gmail.com>
2020-11-22 20:14:50 +0200

    new camera

--- app/src/main/java/com/missclick/fireassistant/ui/dashboard/CameraFragment.kt
+++ app/src/main/java/com/missclick/fireassistant/ui/dashboard/CameraFragment.kt
@@ -3,12 +3,14 @@ package com.missclick.fireassistant.ui.dashboard
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.graphics.ImageFormat
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -18,116 +20,81 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.camerakit.CameraKitView
 import com.missclick.fireassistant.MainActivity
 import com.missclick.fireassistant.R
+import kotlinx.android.synthetic.main.camera_fragment.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class CameraFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CameraFragment()
-    }
+    private lateinit var cameraViewModel: CameraViewModel
+    private lateinit var cameraKitView: CameraKitView
 
-    private lateinit var viewModel: CameraViewModel
-    val LOG_TAG = "myLogs"
-    var myCameras: Array<String>? = null
-    private var mCameraManager: CameraManager? = null
-
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
 
-        if ((activity as MainActivity).checkSelfPermission(Manifest.permission.CAMERA) !== PackageManager.PERMISSION_GRANTED
-            ||
-            ContextCompat.checkSelfPermission(
-                    (activity as MainActivity),
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions(
-                    arrayOf(
-                            Manifest.permission.CAMERA,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE
-                    ), 1
-            )
-        }
         return inflater.inflate(R.layout.camera_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CameraViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mCameraManager = ((activity as MainActivity).getSystemService(Context.CAMERA_SERVICE) as CameraManager?)
-        try{
-            // Получение списка камер с устройства
+        cameraViewModel =
+                ViewModelProvider(this).get(CameraViewModel::class.java)
 
-//            myCameras = new String [mCameraManager.getCameraIdList().length];
+        cameraKitView = camera
 
-            // выводим информацию по камере
-            for ( cameraID in mCameraManager!!.cameraIdList) {
-                Log.i(LOG_TAG, "cameraID: " + cameraID)
-                val id = Integer.parseInt(cameraID)
-
-                // Получениe характеристик камеры
-                val cc = mCameraManager!!.getCameraCharacteristics(cameraID)
-                // Получения списка выходного формата, который поддерживает камера
-                val configurationMap = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-
-                //  Определение какая камера куда смотрит
-                val Faceing = cc.get(CameraCharacteristics.LENS_FACING)
-
-                if (Faceing ==  CameraCharacteristics.LENS_FACING_FRONT)
-                {
-                    Log.i(LOG_TAG, "Camera with ID: " + cameraID + "  is FRONT CAMERA  ");
+        btn.setOnClickListener {
+            cameraKitView.captureImage { cameraKitView, capturedImage ->
+                Log.e("callback", "works")
+                val savedPhoto = File(Environment.getExternalStorageDirectory(), "photo.jpg")
+                try {
+                    val outputStream = FileOutputStream(savedPhoto.getPath())
+                    outputStream.write(capturedImage)
+                    outputStream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
                 }
-
-                if (Faceing ==  CameraCharacteristics.LENS_FACING_BACK)
-                {
-                    Log.i(LOG_TAG, "Camera with: ID " + cameraID + " is BACK CAMERA  ");
-                }
-
-
-                // Получения списка разрешений которые поддерживаются для формата jpeg
-                val sizesJPEG = configurationMap?.getOutputSizes(ImageFormat.JPEG)
-
-                if (sizesJPEG != null) {
-                    for (item in sizesJPEG) {
-                        Log.i(LOG_TAG, "w:" + item.getWidth() + " h:" + item.getHeight())
-                    }
-                }  else {
-                    Log.i(LOG_TAG, "camera don`t support JPEG")
-                }
+                val bitmap = BitmapFactory.decodeByteArray(capturedImage, 0, capturedImage.size)
+                kekWImage.setImageBitmap(bitmap)
             }
         }
-        catch (e: CameraAccessException){
-//            Log.e(LOG_TAG, e.getMessage())
-            e.printStackTrace()
-        }
+    }
 
-        mCameraManager = (activity as MainActivity).getSystemService(Context.CAMERA_SERVICE) as CameraManager?
-        try {
+    override fun onStart() {
+        super.onStart()
+        cameraKitView!!.onStart()
+    }
 
-            // Получение списка камер с устройства
-            myCameras = arrayOf(arrayOfNulls<CameraService>(mCameraManager!!.cameraIdList.size).toString())
-            for (cameraID in mCameraManager!!.cameraIdList) {
-                Log.i(LOG_TAG, "cameraID: $cameraID")
-                val id = cameraID.toInt()
+    override fun onResume() {
+        super.onResume()
+        cameraKitView!!.onResume()
+    }
 
-                // создаем обработчик для камеры
-//                myCameras.get(id) = CameraService(mCameraManager!!, cameraID)
-            }
-        } catch (e: CameraAccessException) {
-            Log.e(LOG_TAG, e.message!!)
-            e.printStackTrace()
-        }
+    override fun onPause() {
+        cameraKitView!!.onPause()
+        super.onPause()
+    }
 
+    override fun onStop() {
+        cameraKitView!!.onStop()
+        super.onStop()
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        cameraKitView!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
 }
